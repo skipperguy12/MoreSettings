5c6514c1462de891a5f8c6d6741bc4e32277d7fe
skipperguy12 <neiljohari@gmail.com>
2013-05-13 08:42:35 -0300

    Removed more unnecessary methods

--- src/main/java/com/github/skipperguy12/MoreSettings/CustomPlayer.java
+++ src/main/java/com/github/skipperguy12/MoreSettings/CustomPlayer.java
@@ -13,48 +13,20 @@ public class CustomPlayer {
 		this.player = player;
 	}
 
-	public void reset() {
-		Player bukkit = getBukkit();
-		bukkit.closeInventory();
-		bukkit.getInventory().clear();
-		bukkit.getInventory().setArmorContents(null);
-		bukkit.setExhaustion(0.0F);
-		bukkit.setFallDistance(0.0F);
-		bukkit.setFireTicks(0);
-		bukkit.setFoodLevel(20);
-		bukkit.setHealth(bukkit.getMaxHealth());
-		bukkit.setLevel(0);
-		bukkit.setExp(0.0F);
-		bukkit.setSaturation(5.0F);
-		bukkit.setSneaking(false);
-		bukkit.setSprinting(false);
-		bukkit.getVehicle().eject();
-		for (PotionEffect effect : bukkit.getActivePotionEffects()) {
-			if (effect.getType() != null) {
-				bukkit.removePotionEffect(effect.getType());
-			}
-		}
-		bukkit.setBedSpawnLocation(null);
-	}
-
 	public Player getBukkit() {
 		return this.player;
-	}
-
-	public void ejectFromVehicle() {
-		this.player.getVehicle().eject();
 	}
 
 	public String getName() {
 		return player.getName();
 	}
 
-	public EntityDamageEvent getLastDamageCause() {
-		return player.getLastDamageCause();
-	}
-
 	public float getFallDistance() {
 		return player.getFallDistance();
+	}
+
+	public EntityDamageEvent getLastDamageCause() {
+		return player.getLastDamageCause();
 	}
 
 }
